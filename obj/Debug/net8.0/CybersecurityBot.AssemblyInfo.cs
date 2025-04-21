87ede38fc83761df04c41bb6caf0e3664407de99
TaylerSUsm <st10445063@imconnect.edu.za>
2025-04-21 03:46:27 +0200

    updated project file
    Had an error in my workflow

--- obj/Debug/net8.0/CybersecurityBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/CybersecurityBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65a066e8b48bbef8486ec5411ecc133d2e578cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
