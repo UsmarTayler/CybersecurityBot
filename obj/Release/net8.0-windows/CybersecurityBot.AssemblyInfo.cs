97b689b187478499e240517f81a301d50346c4d3
TaylerSUsm <st10445063@imconnect.edu.za>
2025-05-22 01:08:55 +0200

    Updated Part 1
    Made adjustments based on feedback from rubrick

--- obj/Release/net8.0-windows/CybersecurityBot.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/CybersecurityBot.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityBot")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327008a4f15fe3136e7b7b8e0e89b47889986bd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityBot")]
